963a8d7c619d302d0c7791502763bf8f881b77e4
bangxu <709463766@qq.com>
2018-07-14 00:04:21 +0800

    init project

---
+++ msclms-web/src/main/java/com/ahau/msclms/web/MyApplication.java
@@ -0,0 +1,18 @@
+package com.ahau.msclms.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author By Barry
+ * @Description: Application launcher,a simple management system for library
+ * @date create in  21:23 2018/7/13
+ */
+@SpringBootApplication
+public class MyApplication  {
+
+    public static void main(String [] args){
+        SpringApplication.run(MyApplication.class,args);
+    }
+
+}
